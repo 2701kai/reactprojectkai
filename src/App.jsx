d7dd6f7634633e505f7392f80dd0cd4e5b7af3b0
2701kai <2701kai@gmail.com>
2025-04-30 00:47:00 +0200

    comp.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import { useState } from "react";
+import TerminalAliases from "../components/pages/aliases/terminal-aliases";
 
 function App() {
+  const [showAliases, setShowAliases] = useState(false);
+  const handleClick = () => {
+    setShowAliases(true);
+  };
+
   return (
     <>
       <img src="./hate.png" alt="Hass!" className="mx-auto" />
-      <h1 className="text-6xl">kai + React:</h1>
+      {/* <h1 className="text-6xl">kai + React:</h1> */}
 
-      {/* <img src="react-whaaat-s.webp" alt="Whaaaat?" className="mx-auto" /> */}
+      <div className=" flex-1/2 p-10">
+        <button onClick={handleClick} className="btn btn-primary">
+          Show Terminal Aliases
+        </button>
 
-      <div className="flex justify-center items-center min-h-screen">
-        <img src="react-whaat-xxl.webp" alt="Whaaaat?" />
+        {showAliases && <TerminalAliases />}
       </div>
+
+      <img src="react-whaaat-s.webp" alt="Whaaaat?" className="mx-auto" />
+
+      {/* <div className="flex justify-center items-center min-h-screen">
+        <img src="react-whaat-xxl.webp" alt="Whaaaat?" />
+      </div> */}
     </>
   );
 }
